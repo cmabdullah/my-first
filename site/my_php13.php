a0d8c5f8d68ef181f2989f797345906564e0d466
Abdullah Khan <a.kium.khan@gmail.com>
2016-06-15 13:50:13 +0600

    My php

---
+++ site/my_php13.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Constant </title>
+</head>
+<body>
+<?php
+/**********************/
+	$Page_number = 560;
+		define("PAGE_NUMBER",560);
+			echo PAGE_NUMBER;
+			echo "<br />";
+	$Page_number += 1 ;
+		echo $Page_number;
+		echo "<br />";
+	$total_page = PAGE_NUMBER +55;
+		echo $total_page;
+
+?>
+</body>
+</html>
